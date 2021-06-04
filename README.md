e826ac8d3ba7926e961b4b5ea6e27d0a88cf8d47
Microsoft Learn Student <learn@contoso.com>
2021-06-04 08:39:26 -0400

    added semantic ui, demo changing input component to button component

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
+#Installing Semantic UI into Create React App 
+
+# Install Gulp
+# Semantic UI uses Gulp to provide command line tools for building themed versions of the# library with just the components you need.
+
+# Gulp is an NPM module and must be installed globally
+
+npm install -g gulp
+
+#Install Semantic UI
+#Semantic UI is available in an eponymous #package on NPM
+
+npm install semantic-ui --save
+cd semantic/
+gulp build
+
+Include in Your HTML
+Running the gulp build tools will compile CSS and Javascript for use in your project. Just link to these files in your HTML along with the latest jQuery.
+
+<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+<script src="semantic/dist/semantic.min.js"></script>
+
+
+Updating via NPM
+Semantic's NPM install script will automatically update Semantic UI to the latest version while preserving your site and packaged themes.
+
+npm update
+
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -13,13 +13,32 @@ const AddTodo = ({ onAdd }) => {
     return (
         <form onSubmit={onSubmit}>
             <label>Add Todo:</label>
-            <input 
-                type="text" 
-                placeholder="Todo Item"
-                value={item}
-                onChange={ (e) => setItem(e.target.value)}
-            /> 
-            <input type="submit" value="Add Todo" />
+           
+{/*            Input field for AddTodo object todo wrapped in a div */}
+           <div>
+                <input 
+                    type="text" 
+                    placeholder="Todo Item"
+                    value={item}
+                    /* setitem() acts as anon function and 
+                    lifts the e.target.value param's value 
+                    up to the current state accross the the app */
+                    onChange={ (e) => setItem(e.target.value)}
+                /> 
+            </div>
+
+{/* button section, 
+    adding sem ui, 
+        changing `<input type="submit">` to `<button type="submit">`
+        New Element <button> must include 
+
+            `type="submit"`, 
+            `value="Add Todo"` 
+            `class="ui button"`
+            `AddTodo` to access the props of the `Add Todo` object */}
+            <div>
+            <button type="submit" value="Add Todo" class="ui button">add must do</button>
+        </div>
         </form>
     )
 }
